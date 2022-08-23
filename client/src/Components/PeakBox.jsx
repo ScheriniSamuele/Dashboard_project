8d04d24c6f9b0093359401915760aa285d951739
Scherini Samuele <samuele.scherini@mail.polimi.it>
2022-08-23 11:13:00 +0200

    dashboard api  and front-nd

--- client/src/Components/PeakBox.jsx
+++ client/src/Components/PeakBox.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const PeakBox = (props) => {
-    const { timePeriod } = props;
+    const { timePeriod, peakValue } = props;
 
     return (
         <div className='dashboard-box dashboard-peak'>
             <h2>{timePeriod}</h2>
+            Peak found at {peakValue.data}
+            <br />
+            value: {peakValue.total}
         </div>
     );
 };
