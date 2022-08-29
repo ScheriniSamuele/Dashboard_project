c298cbf91ccf1afa1918950f2a511b4a223be19e
Scherini Samuele <samuele.scherini@mail.polimi.it>
2022-08-29 15:51:05 +0200

    dashboard done

--- client/src/Components/TimeSlotsBox.jsx
+++ client/src/Components/TimeSlotsBox.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
 const TimeSlotsBox = (props) => {
-    const { timePeriod } = props;
+    const { timePeriod, timeSlotsData } = props;
 
     const renderTimeSlotsValues = () => {
-        return <div>{timePeriod}</div>;
+        if (typeof timeSlotsData[0] !== 'undefined') {
+            const labels = Object.keys(timeSlotsData[0]);
+            const values = Object.values(timeSlotsData[0]);
+
+            const timeSlots = {};
+
+            labels.forEach((element, index) => {
+                timeSlots[element] = values[index].toFixed(3);
+            });
+
+            return Object.keys(timeSlots).map((key, index) => {
+                return (
+                    <div key={index}>
+                        <span className='dashboard-usage-text'>
+                            <span className={key === 'F1' ? 'purpleText' : key === 'F2' ? 'yellowText' : 'redText'}>{key}</span> : {timeSlots[key]}
+                        </span>
+                        /Watt
+                    </div>
+                );
+            });
+        }
     };
 
     return (
